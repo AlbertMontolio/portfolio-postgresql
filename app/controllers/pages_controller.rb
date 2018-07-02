2c2a8a379407ebb2356a628a37528f3de906879a
Albert <albert.montolio@gmail.com>
2018-07-02 22:23:29 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,5 +37,54 @@ class PagesController < ApplicationController
 				pic: "technologies/bootstrap.png"
 			}
 		]
+		blogs
+	end
+
+	def blogs
+		@blogs = [
+			{
+				pic: "200_bootcamp.jpg",
+				title: "What happens if you give 200% in a coding bootcamp",
+				views: "+1000",
+				date: "Jun 29",
+				time: "22 min read"
+			},
+			{
+				pic: "200_bootcamp.jpg",
+				title: "Rails 5 + JavaScript + Webpack: the ultimate guide",
+				views: "+400",
+				date: "Apr 15",
+				time: "18 min read"
+			},
+			{
+				pic: "200_bootcamp.jpg",
+				title: "Build your own Chatbot with Google Dialog Flow",
+				views: "+2000",
+				date: "Mai 27",
+				time: "32 min read"
+			},
+		]
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
