c8b0dbe380db82cada55d07312401317de8fb210
Albert <albert.montolio@gmail.com>
2018-07-03 10:56:06 +0200

    done

--- app/javascript/components/successPic.js
+++ app/javascript/components/successPic.js
@@ -2,15 +2,13 @@ import { isMobileDevice } from '../functions'
 
 
 const successPic = () => {
-	const successPicDesktop = document.querySelector(".success-pic-desktop");
-	const successPicMobile = document.querySelector(".success-pic-mobile");
+	const successPic = document.querySelector(".jsSuccessPic");
 	if (isMobileDevice()) {
 		console.log("i am in mobile device");
-		successPicDesktop.classList.add("my-hide");
-		successPicMobile.classList.remove("my-hide");
+		successPic.classList.remove("parallax");
 	} else {
-		successPicDesktop.classList.remove("my-hide");
-		successPicMobile.classList.add("my-hide");
+		// successPicDesktop.classList.remove("my-hide");
+		// successPicMobile.classList.add("my-hide");
 	}
 	
 }
